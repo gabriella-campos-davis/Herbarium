3f05340a7dc3fc71914de61f02b8b5a61c989808
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2026-02-24 13:39:02 -0600

    Added damaging block behavior to start work on

---
+++ Herbarium/src/BlockBehaviors/BlockBehaviorDamagingPlant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vintagestory.API.Common;
+
+
+namespace Herbarium.src.BlockBehaviors
+{
+    internal class BlockBehaviorDamagingPlant : BlockBehavior
+    {
+        public Block block;
+
+        public BlockBehaviorDamagingPlant(Block block)
+            : base(block)
+        {
+            this.block = block;
+        }
+    }
+}
